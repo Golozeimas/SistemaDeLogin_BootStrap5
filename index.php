7547b5625173cfc8b164b370adb3ec16af660f9f
Matheus <joaosobramatheus@gmail.com>
2025-09-10 14:04:06 -0300

    adicionando pasta imagens

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" data-bs-theme="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Sistema de login </title>
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" >
+</head>
+<body>
+    
+    <div class="d-flex align-items-center py-4 bg-body-tertiary">
+            <main class="w-100 m-autp">
+                <form action="">
+                    <img src="" class="m-4">
+                    <h1>Sead - login</h1>
+                    <input type="" placeholder="">
+                    <input type="" placeholder>
+                </form>
+            </main>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
